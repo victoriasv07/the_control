9d366f015856c56311347a8fe3e8f22213ce564e
LeandroGiovani <leandrogiovani@outlook.com>
2025-07-22 08:06:11 -0300

    ajustes de links com o banco de dados

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 class Config():
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
