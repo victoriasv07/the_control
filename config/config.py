a1efb6a53a6b59994770db40909f1dacbafaa712
Ronaldo Silva Nogueira Batista <ronaldosnbatista@gmail.com>
2025-07-28 23:23:28 -0300

    criação arquivos docker para testes

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 class Config():
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "mysql+pymysql://root:5e5i_123@localhost/the_control")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
